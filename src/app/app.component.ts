55237c79b121ef939a25bf71515cd52a58d4a7d0
Alain BOUDARD <aboudard@connectiv-it.com>
2023-06-29 21:39:12 +0200

    feat: upgrade ng9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Store, select } from '@ngrx/store';
-import { AppState } from './reducers';
-import { increment, decrement, storeCounter } from './actions/counter.actions';
-import { getCount } from './selectors/counter.selector';
-import { Observable, interval } from 'rxjs';
-import { StorageMap } from '@ngx-pwa/local-storage';
+import { Component, OnInit } from "@angular/core";
+import { Store, select } from "@ngrx/store";
+import { AppState } from "./reducers";
+import { increment, decrement, storeCounter } from "./actions/counter.actions";
+import { getCount } from "./selectors/counter.selector";
+import { Observable, combineLatest, interval } from "rxjs";
+import { map } from "rxjs/operators";
+import { StorageMap } from "@ngx-pwa/local-storage";
+
+interface ViewModel {
+  count: number;
+  storage: number;
+}
 
 @Component({
-  selector: 'app-component',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-component",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent implements OnInit {
-  name = 'Angular & NgRx';
-  count$: Observable<number>;
-  storage$: Observable<any>;
+  name = "Angular & NgRx";
+  viewModel$: Observable<ViewModel>;
 
   constructor(private store: Store<AppState>, private storage: StorageMap) {
-    this.count$ = this.store.pipe(select(getCount));
-    this.storage$ = this.storage.get('count');
+    this.viewModel$ = combineLatest([
+      this.store.pipe(select(getCount)),
+      this.storage.get<number>("count") as Observable<number>,
+    ]).pipe(map(([count, storage]) => ({ count, storage })));
   }
 
   decrement(): void {
