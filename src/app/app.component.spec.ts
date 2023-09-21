6d5cad2bb433d3185ce201645dcc017f1ea2cff8
Alain BOUDARD <aboudard@connectiv-it.com>
2023-09-21 16:41:48 +0200

    feat: upgrade ng12

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,7 @@
-import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
-import { Store, StoreModule } from "@ngrx/store";
+import { TestBed, waitForAsync } from "@angular/core/testing";
 import { MockStore, provideMockStore } from "@ngrx/store/testing";
 import * as counterActions from "./actions/counter.actions";
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
 import * as fromRoot from "./reducers";
 import { ObserverSpy, subscribeSpyTo } from "@hirez_io/observer-spy";
 
@@ -14,10 +12,8 @@ describe("AppComponent", () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [StoreModule.forRoot(fromRoot.reducers)],
       providers: [AppComponent, provideMockStore()],
     });
-
     store = TestBed.inject(MockStore);
     storeSpy = subscribeSpyTo(store.scannedActions$);
     component = TestBed.inject(AppComponent);
@@ -31,9 +27,17 @@ describe("AppComponent", () => {
     expect(component.name).toEqual("Angular & NgRx");
   });
 
-  it("should dispatch an action to load data when created", () => {
+  it("should dispatch an action onInit - method 1", () => {
     const action = counterActions.increment();
     component.ngOnInit();
     expect(storeSpy.getLastValue()).toEqual(action);
   });
+
+  it("should dispatch an action onInit - method 2", () => {
+    const spyStore = spyOn(store, "dispatch").and.callThrough();
+    const action = counterActions.increment();
+    component.ngOnInit();
+    expect(spyStore).toHaveBeenCalledWith(action);
+  });
+
 });
